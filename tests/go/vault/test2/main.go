f39554e127d63156d4702bc9277dca3b9fb4cebc
Lei Tang <32078630+lei-tang@users.noreply.github.com>
2018-04-10 15:27:53 -0700

    Add an example csr

--- tests/go/vault/test2/main.go
+++ tests/go/vault/test2/main.go
@@ -1,110 +1,95 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
 	"github.com/hashicorp/vault/api"
+	"io/ioutil"
+	"istio.io/istio/pkg/log"
 )
 
 const (
-	testCsrFile = "testdata/workload-1.csr"
+	vaultAddrForTesting = "http://127.0.0.1:8200"
+	tokenForTesting     = "myroot"
+	testCsrFile         = "testdata/workload-1.csr"
+	workloadRolePath    = "istio_ca/roles/workload_role"
+	signCsrPath         = "istio_ca/sign-verbatim"
 )
 
-func setWorkloadRole() {
-	//pathArg := "cubbyhole/mysecret"
-	pathArg := "istio_ca/roles/workload_role"
+// Get the connection to a Vault server and set the token for the connection
+func getVaultConnection(addr string, token string) (*api.Logical, error) {
+	config := api.DefaultConfig()
+	config.Address = addr
 
-	vaultCFG := api.DefaultConfig()
-	vaultCFG.Address = "http://127.0.0.1:8200"
-
-	var err error
-	vClient, err := api.NewClient(vaultCFG)
+	client, err := api.NewClient(config)
 	if err != nil {
-		log.Fatal(err)
+		log.Errorf("NewClient() failed (error %v)", err)
+		return nil, err
 	}
 
-	vClient.SetToken("myroot")
-	vault := vClient.Logical()
-
-	m := make(map[string]interface{})
-	//vault write istio_ca/roles/workload_role max_ttl=1h key_bits=2048 enforce_hostnames=false allow_any_name=true
-	m["max_ttl"] = "1h"
-	m["key_bits"] = 2048
-	m["enforce_hostnames"] = false
-	m["allow_any_name"] = true
-
-	//_, err = vault.Write(pathArg, m)
-	_, err = vault.Write(pathArg, m)
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Printf("Write succeeds.")
-	}
-
-	s, err := vault.Read(pathArg)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if s == nil {
-		log.Fatal("secret was nil")
-	}
-
-  log.Printf("%#v", *s)
+	//Simply sets the token for future requests without actual authentication
+	client.SetToken(token)
+	return client.Logical(), nil
 }
 
-func signCsr() {
-		//pathArg := "cubbyhole/mysecret"
-	pathArg := "istio_ca/sign-verbatim"
-
-	vaultCFG := api.DefaultConfig()
-	vaultCFG.Address = "http://127.0.0.1:8200"
-
-	var err error
-	vClient, err := api.NewClient(vaultCFG)
-	if err != nil {
-		log.Fatal(err)
+// Set the workload role with the given max-TTL and number of key bits
+func setWorkloadRole(conn *api.Logical, maxTtl string, keyBits int) error {
+	m := map[string]interface{}{
+		"max_ttl":           maxTtl,
+		"key_bits":          keyBits,
+		"enforce_hostnames": false,
+		"allow_any_name":    true,
 	}
 
-	vClient.SetToken("myroot")
-	vault := vClient.Logical()
+	_, err := conn.Write(workloadRolePath, m)
+	if err != nil {
+		log.Errorf("Write() failed (error %v)", err)
+		return err
+	} else {
+		return nil
+	}
+}
+
+// Sign a CSR and return the signed certificate
+func signCsr(conn *api.Logical, csr string) (*api.Secret, error) {
+	m := map[string]interface{}{
+		"name":                "workload_role",
+		"format":              "pem",
+		"use_csr_common_name": true,
+		"csr": csr,
+	}
+
+	res, err := conn.Write(signCsrPath, m)
+	if err != nil {
+		log.Errorf("Write() failed (error %v)", err)
+		return nil, err
+	} else {
+		return res, nil
+	}
+}
+
+
+// For logging,  -stderrthreshold=INFO --alsologtostderr
+// https://github.com/istio/istio/blob/master/security/cmd/istio_ca/main.go#L243
+func main() {
+	log.Debug("Start testing.")
+	conn, err := getVaultConnection(vaultAddrForTesting, tokenForTesting)
+	if err != nil {
+		log.Errorf("getVaultConnection() failed (error %v)", err)
+		return
+	}
+
+	setWorkloadRole(conn, "1h", 2048)
 
 	testCsr, err := ioutil.ReadFile(testCsrFile)
 	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Printf("ReadFile() succeeds.")
-		log.Printf("%s", testCsr)
+		log.Errorf("ReadFile() failed (error %v)", err)
+		return
 	}
-
-//  secret := make(map[string]interface{})
-//	secret["value"] = "test secret"
-	m := make(map[string]interface{})
-	m["name"] = "workload_role"
-	m["format"] = "pem"
-	m["use_csr_common_name"] = true
-	m["csr"] = string(testCsr[:])
-
-	//_, err = vault.Write(pathArg, m)
-	res, err := vault.Write(pathArg, m)
+	res, err := signCsr(conn, string(testCsr[:]))
 	if err != nil {
-		log.Fatal(err)
+		log.Errorf("signCsr() failed (error %v)", err)
 	} else {
-		log.Printf("Write succeeds.")
-		log.Printf("%#v", *res)
+		log.Debug("signCsr() succeeds.")
+		log.Debugf("%#v", *res)
 	}
-
-	//s, err := vault.Read(pathArg)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//if s == nil {
-	//	log.Fatal("secret was nil")
-	//}
-	//
-	//log.Printf("%#v", *s)
 }
 
-func main() {
-	setWorkloadRole()
-	//signCsr()
-}
