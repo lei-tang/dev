067b5197bb3a4a37c58ad4679a37dfb4720c5ad9
Lei Tang <32078630+lei-tang@users.noreply.github.com>
2018-04-10 21:24:15 -0700

    Add the CA pem file and other functions for signing

--- tests/go/vault/test2/main.go
+++ tests/go/vault/test2/main.go
@@ -4,18 +4,28 @@ import (
 	"github.com/hashicorp/vault/api"
 	"io/ioutil"
 	"istio.io/istio/pkg/log"
+	"fmt"
+//	"k8s.io/kubernetes/pkg/util/mount"
 )
 
 const (
-	vaultAddrForTesting = "http://127.0.0.1:8200"
-	tokenForTesting     = "myroot"
-	testCsrFile         = "testdata/workload-1.csr"
+	istioCAMountPoint   = "istio_ca"
+	istioCADescription   = "Istio CA"
+	configCAKeyCertPath = "istio_ca/config/ca"
 	workloadRolePath    = "istio_ca/roles/workload_role"
 	signCsrPath         = "istio_ca/sign-verbatim"
 )
 
+// Config for testing purpose
+const (
+	vaultAddrForTesting = "http://127.0.0.1:8200"
+	tokenForTesting     = "myroot"
+	testCAKeyCertFile   = "testdata/istio_ca.pem"
+	testCsrFile         = "testdata/workload-1.csr"
+)
+
 // Get the connection to a Vault server and set the token for the connection
-func getVaultConnection(addr string, token string) (*api.Logical, error) {
+func getVaultConnection(addr string, token string) (*api.Client, error) {
 	config := api.DefaultConfig()
 	config.Address = addr
 
@@ -27,11 +37,11 @@ func getVaultConnection(addr string, token string) (*api.Logical, error) {
 
 	//Simply sets the token for future requests without actual authentication
 	client.SetToken(token)
-	return client.Logical(), nil
+	return client, nil
 }
 
 // Set the workload role with the given max-TTL and number of key bits
-func setWorkloadRole(conn *api.Logical, maxTtl string, keyBits int) error {
+func setWorkloadRole(client *api.Client, maxTtl string, keyBits int) error {
 	m := map[string]interface{}{
 		"max_ttl":           maxTtl,
 		"key_bits":          keyBits,
@@ -39,7 +49,7 @@ func setWorkloadRole(conn *api.Logical, maxTtl string, keyBits int) error {
 		"allow_any_name":    true,
 	}
 
-	_, err := conn.Write(workloadRolePath, m)
+	_, err := client.Logical().Write(workloadRolePath, m)
 	if err != nil {
 		log.Errorf("Write() failed (error %v)", err)
 		return err
@@ -48,8 +58,23 @@ func setWorkloadRole(conn *api.Logical, maxTtl string, keyBits int) error {
 	}
 }
 
+// Set the certificate and the private key of the CA
+func setCAKeyCert(client *api.Client, keyCert string) (*api.Secret, error) {
+	m := map[string]interface{}{
+		"pem_bundle": keyCert,
+	}
+
+	res, err := client.Logical().Write(configCAKeyCertPath, m)
+	if err != nil {
+		log.Errorf("Write() failed (error %v)", err)
+		return nil, err
+	} else {
+		return res, nil
+	}
+}
+
 // Sign a CSR and return the signed certificate
-func signCsr(conn *api.Logical, csr string) (*api.Secret, error) {
+func signCsr(client *api.Client, csr string) (*api.Secret, error) {
 	m := map[string]interface{}{
 		"name":                "workload_role",
 		"format":              "pem",
@@ -57,7 +82,7 @@ func signCsr(conn *api.Logical, csr string) (*api.Secret, error) {
 		"csr": csr,
 	}
 
-	res, err := conn.Write(signCsrPath, m)
+	res, err := client.Logical().Write(signCsrPath, m)
 	if err != nil {
 		log.Errorf("Write() failed (error %v)", err)
 		return nil, err
@@ -69,27 +94,65 @@ func signCsr(conn *api.Logical, csr string) (*api.Secret, error) {
 
 // For logging,  -stderrthreshold=INFO --alsologtostderr
 // https://github.com/istio/istio/blob/master/security/cmd/istio_ca/main.go#L243
+
+//vault mount -path=istio_ca -description="Istio CA" pki
+//vault write istio_ca/config/ca  pem_bundle=@./istio_ca.pem
+//vault write istio_ca/roles/workload_role max_ttl=1h key_bits=2048 enforce_hostnames=false allow_any_name=true
+//vault write istio_ca/sign-verbatim name=workload_role format=pem use_csr_common_name=true csr=@workload-1.csr
 func main() {
 	log.Debug("Start testing.")
-	conn, err := getVaultConnection(vaultAddrForTesting, tokenForTesting)
+	client, err := getVaultConnection(vaultAddrForTesting, tokenForTesting)
 	if err != nil {
 		log.Errorf("getVaultConnection() failed (error %v)", err)
 		return
 	}
 
-	setWorkloadRole(conn, "1h", 2048)
+	var mountInput api.MountInput
+	mountInput.Description = istioCADescription
+	mountInput.Type = "pki"
+	// Mount the Istio PKI
+	err = client.Sys().Mount(istioCAMountPoint, &mountInput)
+	if err != nil {
+		log.Errorf("Mount() failed (error %v)", err)
+		return
+	}
 
+	// Call setCAKeyCert()
+	keyCert, err := ioutil.ReadFile(testCAKeyCertFile)
+	if err != nil {
+		log.Errorf("ReadFile() failed (error %v)", err)
+		return
+	}
+	_, err = setCAKeyCert(client, string(keyCert[:]))
+	if err != nil {
+		log.Errorf("setCAKeyCert() failed (error %v)", err)
+	} else {
+		log.Debug("setCAKeyCert() succeeds.")
+		//log.Debugf("%#v", *res)
+		//fmt.Printf("%v", res.Data["certificate"])
+		//fmt.Printf("%#v", *res)
+	}
+
+	// Call setWorkloadRole()
+	err = setWorkloadRole(client, "1h", 2048)
+	if err != nil {
+		log.Errorf("setWorkloadRole() failed (error %v)", err)
+		return
+	}
+
+	// Call signCsr()
 	testCsr, err := ioutil.ReadFile(testCsrFile)
 	if err != nil {
 		log.Errorf("ReadFile() failed (error %v)", err)
 		return
 	}
-	res, err := signCsr(conn, string(testCsr[:]))
+	res, err := signCsr(client, string(testCsr[:]))
 	if err != nil {
 		log.Errorf("signCsr() failed (error %v)", err)
 	} else {
 		log.Debug("signCsr() succeeds.")
 		log.Debugf("%#v", *res)
+		fmt.Printf("%v", res.Data["certificate"])
+		//fmt.Printf("%#v", *res)
 	}
 }
-
