3a61abeb3c2fc7b9d591586603f496ee0308b08e
Lei Tang <32078630+lei-tang@users.noreply.github.com>
2018-10-01 12:21:45 -0700

    Remove unused script and change OIDC server name

--- tests/go/group-demo-2/script/run_demo.sh
+++ tests/go/group-demo-2/script/run_demo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-### Start OIDC server
+### Start Claims-Provider server
 pushd ~/go/src/github.com/lei-tang/dev/tests/go/group-demo-2/oidc_server
 go run oidc_server.go -logtostderr
 # Open a new terminal and export the following environmental variables
-export TLS_CERT_PATH="The TLS cert path outputed by the oidc server"
-export JWT="The example JWT outputed by the oidc server"
+export TLS_CERT_PATH="The TLS cert path outputed by the claims-provider server"
+export JWT="The example JWT outputed by the claims-provider server"
 
 ### Config RBAC policy for httpbin service to require a JWT with valid groups claim
 export NS=rbac-groups-test-ns
